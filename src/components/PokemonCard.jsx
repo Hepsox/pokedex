bd6e043941f0da08a80626fdb97f541b77deb5a7
Hepsox <marie.delaire@outlook.com>
2023-10-10 11:28:37 +0200

    props1

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,27 +1,13 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-function PokemonCard() {
-  const pokemon = pokemonList[1];
+function PokemonCard({ pokemon }) {
+  console.log(pokemon);
 
   return (
-    <figure>
-      {pokemon.imgSrc === undefined ? (
-        <p>????</p>
-      ) : (
-        <img src={pokemon.imgSrc} />
-      )}
-
-      <figcaption> {pokemon.name} </figcaption>
-    </figure>
+    <div>
+      <figure>
+        <figcaption> {pokemon.name}</figcaption>
+        {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p> ??? </p>}
+      </figure>
+    </div>
   );
 }
 
